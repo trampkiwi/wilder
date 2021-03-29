fd099169ebad718afd6e669d97d7dd8274320663
Youngjun Lee <starwatcherkiwi@gmail.com>
2021-03-30 00:05:38 +0100

    edit

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ $(function(){
     // Initialise map
 
     var map = L.map('mapdiv').setView([51.505, -0.09], 13);
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    L.tileLayer('https://api.mapbox.com/styles/v1/{username}/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
-        //username: 'starwatcherkiwi',
-        //id: 'ckmv4oewx06sw17p7ezxkfw5h',
-        id: 'mapbox/streets-v11',
+        username: 'starwatcherkiwi',
+        id: 'ckmv4oewx06sw17p7ezxkfw5h',
         tileSize: 512,
         zoomOffset: -1,
         accessToken: 'pk.eyJ1Ijoic3RhcndhdGNoZXJraXdpIiwiYSI6ImNrbXY0ODVkMjAxa2Myb205ZTFxeDVweHEifQ.fwIZz2Ljwvpv3dGH7Elp9g'
@@ -15,7 +14,7 @@ $(function(){
 
     // Locate user
 
-    /*map.locate({setView: true, maxZoom: 16});
+    map.locate({setView: true, maxZoom: 16});
     
     function onLocationFound(e) {
         var err = e.accuracy;
@@ -31,5 +30,5 @@ $(function(){
         alert(e.message);
     }
     
-    map.on('locationerror', onLocationError);*/
+    map.on('locationerror', onLocationError);
 });
