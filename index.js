870564e59f6d30f838bab098e0f7507503d44624
Youngjun Lee <starwatcherkiwi@gmail.com>
2021-03-29 23:55:12 +0100

    edit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $(function(){
     // Initialise map
 
-    var map = L.map('mapdiv').setView([51.505, -0.09], 13);
+    var map = L.map('mapdiv');
     L.tileLayer('https://api.mapbox.com/styles/v1/{username}/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
