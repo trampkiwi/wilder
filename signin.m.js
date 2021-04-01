947623b6ea5b5a3f844f00a507533e2d12282e0d
Youngjun Lee <starwatcherkiwi@gmail.com>
2021-04-01 19:06:04 +0100

    general modificaation

--- signin.m.js
+++ signin.m.js
@@ -1,9 +1,21 @@
+var veil;
+var navBar;
+
 $(function() {
-    $('#openNavbar').on('click', function() {
-        $('.navBar').css('height', '70%');
+    veil = $('.veil');
+    navBar = $('.navBar');
+
+    $('#openNavbar').on('click', () => {
+        veil.css('display', 'block');
+        veil.css('background-color', 'rgba(0, 0, 0, 0.5)');
+        navBar.css('height', '70%');
     });
 
-    $('#closeNavbar').on('click', function() {
-        $('.navBar').css('height', '0');
+    $('#closeNavbar').on('click', () => {
+        veil.css('background-color', 'rgba(0, 0, 0, 0)');
+        veil.on('transitionend', () => {
+            veil.css('display', 'none');
+        })
+        navBar.css('height', '0');
     });
 });
