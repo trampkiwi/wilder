256c716f2403b1ca5ab23fcf9fca33d644150ea1
Youngjun Lee <starwatcherkiwi@gmail.com>
2021-04-04 04:04:26 +0100

    Fixed signin_complete.m.html behaviour

---
+++ firebase-init.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBDEc8_oSw_vVlZt1MkQd7j_zPbZv370zQ",
+    authDomain: "wilder-b612a.firebaseapp.com",
+    databaseURL: "https://wilder-b612a-default-rtdb.firebaseio.com",
+    projectId: "wilder-b612a",
+    storageBucket: "wilder-b612a.appspot.com",
+    messagingSenderId: "821439630051",
+    appId: "1:821439630051:web:6492a72602a5dbfa7c93d6",
+    measurementId: "G-3YKPD99FCT"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
