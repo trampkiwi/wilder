290bc20117ae41de204ce1918109a2f3bcb2e784
Youngjun Lee <starwatcherkiwi@gmail.com>
2021-04-04 04:18:29 +0100

    Added firebase auth test snippet to index.m.html

--- validate_user.js
+++ validate_user.js
@@ -1,13 +1,19 @@
 // Import after firebase-database.js
 
-var db = firebase.database();
+firebase.auth().onAuthStateChanged((user) => {
+    if(user) { // User logged in
+        var db = firebase.database();
 
-// Only logged in and 'allowed' users can access the dummy document.
-
-db.ref('/user_validation_dummy').once('value').then(() => {
-    // Do nothing
-}).catch((err) => { // If error occurs
-    if(err.code == 'PERMISSION_DENIED') { // Current login is invalid.
-        window.location.replace('/invalid_user.m.html'); // Redirect to invalid user page.
+        // Only logged in and 'allowed' users can access the dummy document.
+        db.ref('/user_validation_dummy').once('value').then(() => {
+            // Do nothing
+        }).catch((err) => { // If error occurs
+            if(err.code == 'PERMISSION_DENIED') { // Current login is invalid.
+                window.location.replace('/invalid_user.m.html'); // Redirect to invalid user page.
+            }
+        });
+    } else { // User not logged in
+        window.location.replace('/');
     }
 });
+
