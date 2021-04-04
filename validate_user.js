0b0d36bc7f43b84e333e04880bf891c5a4d3f174
Youngjun Lee <starwatcherkiwi@gmail.com>
2021-04-04 03:58:52 +0100

    Fixing invalid user redirect behaviour

--- validate_user.js
+++ validate_user.js
@@ -15,7 +15,7 @@ var db = firebase.database();
 db.ref('/user_validation_dummy').once('value').then(() => {
     // Do nothing
 }).catch((err) => { // If error occurs
-    if(err.code == 'permission_denied') { // Current login is invalid.
+    if(err.code == 'PERMISSION_DENIED') { // Current login is invalid.
         window.location.replace('/invalid_user.m.html'); // Redirect to invalid user page.
     }
 });
