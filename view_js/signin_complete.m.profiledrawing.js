ab2f2423a2345ed79f1fbcc3ee14994c5ce353e5
Youngjun Lee <starwatcherkiwi@gmail.com>
2021-04-07 15:57:57 +0100

    developing profile picture drawing view

--- view_js/signin_complete.m.profiledrawing.js
+++ view_js/signin_complete.m.profiledrawing.js
@@ -1,7 +1,12 @@
 var cvsElem;
+var cvsHistoryElem;
 var ctx;
+var historyCtx;
 var hasInitialised = false;
 var isSamplingColour = false;
+var isFillMode = false;
+
+var cCElem;
 
 var pPrevTouch = { x: null, y: null };
 var prevTouch = { x: null, y: null };
@@ -11,8 +16,100 @@ var touchCoords = {};
 
 var scale; // Device pixel ratio
 
+function activateSamplingMode() {
+    $('.sample_colour').css('background-color', 'rgb(22, 22, 22)');
+    $('.sample_colour .black').css('display', 'none');
+    $('.sample_colour .white').css('display', 'inline-block');
+
+    isSamplingColour = true;
+}
+
+function deactivateSamplingMode() {
+    $('.sample_colour').css('background-color', 'rgba(0, 0, 0, 0)');
+    $('.sample_colour .black').css('display', 'inline-block');
+    $('.sample_colour .white').css('display', 'none');
+
+    isSamplingColour = false;
+}
+
+function activateFillMode() {
+    $('.flood_fill').css('background-color', 'rgb(22, 22, 22)');
+    $('.flood_fill .black').css('display', 'none');
+    $('.flood_fill .white').css('display', 'inline-block');
+
+    isFillMode = true;
+}
+
+function deactivateFillMode() {
+    $('.flood_fill').css('background-color', 'rgba(0, 0, 0, 0)');
+    $('.flood_fill .black').css('display', 'inline-block');
+    $('.flood_fill .white').css('display', 'none');
+
+    isFillMode = false;
+}
+
+function activateBrushSize(elem) {
+    $(elem).css('background-color', 'rgb(22, 22, 22)');
+    $(elem).children('div').css('background-color', 'white');
+
+    ctx.lineWidth = parseInt($(elem).attr('brush_weight')) * scale;
+}
+
+function deactivateBrushSize(elem) {
+    $(elem).css('background-color', 'rgba(0, 0, 0, 0)');
+    $(elem).children('div').css('background-color', 'rgb(22, 22, 22)');
+}
+
+function drawCallback(ev) {
+    ev.preventDefault();
+
+    touchCoords.x = ev.touches[0].pageX - cvsOffsetCoords.x;
+    touchCoords.y = ev.touches[0].pageY - cvsOffsetCoords.y;
+
+    var pPrevTouchExists = pPrevTouch.x != null && pPrevTouch.y != null;
+    var prevTouchExists = prevTouch.x != null & prevTouch.y != null;
+    
+    if(pPrevTouchExists) {
+        ctx.beginPath();
+        ctx.moveTo(pPrevTouch.x * scale, pPrevTouch.y * scale);
+        ctx.lineTo(prevTouch.x * scale, prevTouch.y * scale);
+        ctx.lineTo(touchCoords.x * scale, touchCoords.y * scale);
+        ctx.stroke();
+    } else if(prevTouchExists) {
+        ctx.beginPath();
+        ctx.moveTo(prevTouch.x * scale, prevTouch.y * scale);
+        ctx.lineTo(touchCoords.x * scale, touchCoords.y * scale);
+        ctx.stroke();
+    }
+
+    pPrevTouch.x = prevTouch.x;
+    pPrevTouch.y = prevTouch.y;
+
+    prevTouch.x = touchCoords.x;
+    prevTouch.y = touchCoords.y;
+}
+
+function sampleColourCallback(ev) {
+    ev.preventDefault();
+
+    touchCoords.x = ev.touches[0].pageX - cvsOffsetCoords.x;
+    touchCoords.y = ev.touches[0].pageY - cvsOffsetCoords.y;
+
+    var sampledColour = ctx.getImageData(touchCoords.x * scale, touchCoords.y * scale, 1, 1).data;
+
+    var sampledColourReg = [];
+
+    for(var i = 0; i < 3; i++) { sampledColourReg.push(sampledColour[i] / 255); }
+
+    ctx.strokeStyle = `rgb(${sampledColour[0]}, ${sampledColour[1]}, ${sampledColour[2]})`;
+
+    cCElem.attr('colour_reg_hsluv', regulariseHSLuv(hsluv.Hsluv.rgbToHsluv(sampledColourReg)).toString());
+    cCElem.css('background-color', `rgb(${sampledColour[0]}, ${sampledColour[1]}, ${sampledColour[2]})`);
+}
+
 function initialiseProfileDrawingView() {
     cvsElem = $('#drawing_canvas');
+    cvsHistoryElem = $('#drawing_canvas_history');
 
     $('.profile_pic_display').on('click', (e) => {
         modalView.openModal($('#drawing_view'), $('#drawing_view_veil'), '100%').then(() => {
@@ -23,19 +120,15 @@ function initialiseProfileDrawingView() {
 
                 scale = window.devicePixelRatio;
 
-                if(containerHeight > containerWidth) {
-                    cvsElem.css('width', containerWidth);
-                    cvsElem.css('height', containerWidth);
+                var dimension = Math.min(containerWidth, containerHeight);
 
-                    cvsElem.attr('width', containerWidth * scale);
-                    cvsElem.attr('height', containerWidth * scale);
-                } else {
-                    cvsElem.css('width', containerHeight);
-                    cvsElem.css('height', containerHeight);
+                cvsElem.css('width', dimension);
+                cvsElem.css('height', dimension);
 
-                    cvsElem.attr('width', containerHeight * scale);
-                    cvsElem.attr('height', containerHeight * scale);
-                }
+                cvsElem.attr('width', dimension * scale);
+                cvsElem.attr('height', dimension * scale);
+                cvsHistoryElem.attr('width', dimension * scale);
+                cvsHistoryElem.attr('height', dimension * scale);
 
                 cvsElem.css('display', 'inline-block');
 
@@ -44,77 +137,117 @@ function initialiseProfileDrawingView() {
                 ctx.fillStyle = '#ffffff';
                 ctx.fillRect(0, 0, cvsElem.attr('width'), cvsElem.attr('height'));
 
+                ctx.lineJoin = 'round';
+                ctx.lineCap = 'round';
+
+                historyCtx = cvsHistoryElem.get(0).getContext('2d');
+
+                historyCtx.fillStyle = '#ffffff';
+                historyCtx.fillRect(0, 0, cvsHistoryElem.attr('width'), cvsHistoryElem.attr('height'));
+
+                ctx.lineWidth = 3 * scale;
+
                 hasInitialised = true;
             }
         });
     });
 
-    $('#drawing_view .closeNavbar').on('click', (e) => {
-        modalView.closeModal($('#drawing_view'), $('#drawing_view_veil'));
-    })
+    var closeDrawingViewCallback = function() { // When drawing view is closed
+        // Save canvas content to img tag
+        $('.profile_pic_display').attr('src', cvsElem.get(0).toDataURL());
 
-    $('#drawing_view_veil').on('click', (e) => {
+        // Close modal
         modalView.closeModal($('#drawing_view'), $('#drawing_view_veil'));
-    });
+    };
+
+    $('#drawing_view .closeNavbar').on('click', closeDrawingViewCallback);
+
+    $('#drawing_view_veil').on('click', closeDrawingViewCallback);
 
     $('.sample_colour').on('click', (e) => {
         if(!isSamplingColour) { // If view is not currently in sampling mode
-            $('.sample_colour').css('background-color', 'black');
-            $('.sample_colour .black').css('display', 'none');
-            $('.sample_colour .white').css('display', 'inline-block');
-
-            isSamplingColour = true;
+            activateSamplingMode();
+            deactivateFillMode();
         } else {
-            $('.sample_colour').css('background-color', 'rgba(0, 0, 0, 0)');
-            $('.sample_colour .black').css('display', 'inline-block');
-            $('.sample_colour .white').css('display', 'none');
-
-            isSamplingColour = false;
+            deactivateSamplingMode();
         }
     });
+
+    $('.flood_fill').on('click', (e) => {
+        if(!isFillMode) { // If view is not currently in flood fill mode
+            activateFillMode();
+            deactivateSamplingMode();
+        } else {
+            deactivateFillMode();
+        }
+    });
+
+    $('.undo').on('click', (e) => { // Load state from history canvas
+        ctx.drawImage(cvsHistoryElem.get(0), 0, 0);
+
+        deactivateSamplingMode();
+        deactivateFillMode();
+    });
+
+    $('.brush_size').on('click', (e) => { // When brush size tool is clicked
+        $('.brush_size').each((i, elem) => {
+            deactivateBrushSize(elem);
+        });
+
+        activateBrushSize(e.target);
+    })
 
     cvsElem.on('touchstart', (e) => {
         cvsOffset = cvsElem.get(0).getBoundingClientRect();
         cvsOffsetCoords.x = cvsOffset.x;
         cvsOffsetCoords.y = cvsOffset.y;
 
-        if(!isSamplingColour) {
-            ctx.lineWidth = 15;
-            ctx.lineJoin = 'round';
-            ctx.lineCap = 'round';
-            ctx.strokeStyle = $('.current_colour').css('background-color');
-
-            var drawCallback = function(ev) {
-                ev.preventDefault();
-
-                touchCoords.x = ev.touches[0].pageX - cvsOffsetCoords.x;
-                touchCoords.y = ev.touches[0].pageY - cvsOffsetCoords.y;
-
-                var pPrevTouchExists = pPrevTouch.x != null && pPrevTouch.y != null;
-                var prevTouchExists = prevTouch.x != null & prevTouch.y != null;
-                
-                if(pPrevTouchExists) {
-                    ctx.beginPath();
-                    ctx.moveTo(pPrevTouch.x * scale, pPrevTouch.y * scale);
-                    ctx.lineTo(prevTouch.x * scale, prevTouch.y * scale);
-                    ctx.lineTo(touchCoords.x * scale, touchCoords.y * scale);
-                    ctx.stroke();
-                } else if(prevTouchExists) {
-                    ctx.beginPath();
-                    ctx.moveTo(prevTouch.x * scale, prevTouch.y * scale);
-                    ctx.lineTo(touchCoords.x * scale, touchCoords.y * scale);
-                    ctx.stroke();
-                }
-
-                pPrevTouch.x = prevTouch.x;
-                pPrevTouch.y = prevTouch.y;
-
-                prevTouch.x = touchCoords.x;
-                prevTouch.y = touchCoords.y;
-            }
+        if(isSamplingColour) { // Sampling mode
+            cCElem = $('.current_colour');
+            var cRegHsl = cCElem.attr('colour_reg_hsluv').split(',').map((v) => {
+                return parseFloat(v);
+            });
+            addPastColour(cRegHsl);         
 
             $(window).on('touchend', () => {
                 cvsElem.off('touchmove');
+
+                deactivateSamplingMode();
+            });
+
+            cvsElem.on('touchmove', sampleColourCallback);
+            sampleColourCallback(e);
+        } else if(isFillMode) { // Fill mode
+            // Save current sate in hidden history canvas
+
+            historyCtx.drawImage(cvsElem.get(0), 0, 0);
+
+            // Clear event handlers
+
+            $(window).off('touchend');
+            cvsElem.off('touchmove');
+
+            // Fill canvas
+
+            ctx.fillStyle = $('.current_colour').css('background-color');
+            ctx.fillRect(0, 0, cvsElem.attr('width'), cvsElem.attr('height'));
+
+            // Toggle fill mode
+
+            deactivateFillMode();
+        } else { // Drawing mode
+
+            // Save current sate in hidden history canvas
+
+            historyCtx.drawImage(cvsElem.get(0), 0, 0);
+
+            // Canvas init
+
+            ctx.strokeStyle = $('.current_colour').css('background-color');
+
+            $(window).on('touchend', () => {
+                cvsElem.off('touchmove');
+
                 prevTouch.x = null;
                 prevTouch.y = null;
                 pPrevTouch.x = null;
@@ -123,48 +256,6 @@ function initialiseProfileDrawingView() {
     
             cvsElem.on('touchmove', drawCallback);
             drawCallback(e);
-        } else {
-            var cCElem = $('.current_colour');
-            var cRegHsl = cCElem.attr('colour_reg_hsluv').split(',').map((v) => {
-                return parseFloat(v);
-            });
-            addPastColour(cRegHsl);         
-
-            var sampleColourCallback = function(ev) {
-                ev.preventDefault();
-
-                touchCoords.x = ev.touches[0].pageX - cvsOffsetCoords.x;
-                touchCoords.y = ev.touches[0].pageY - cvsOffsetCoords.y;
-
-                var sampledColour = ctx.getImageData(touchCoords.x * scale, touchCoords.y * scale, 1, 1).data;
-
-                var sampledColourReg = [];
-
-                for(var i = 0; i < 3; i++) { sampledColourReg.push(sampledColour[i] / 255); }
-
-                console.log(sampledColour);
-                console.log(sampledColourReg);
-
-                ctx.strokeStyle = `rgb(${sampledColour[0]}, ${sampledColour[1]}, ${sampledColour[2]})`;
-
-                cCElem.attr('colour_reg_hsluv', regulariseHSLuv(hsluv.Hsluv.rgbToHsluv(sampledColourReg)).toString());
-                cCElem.css('background-color', `rgb(${sampledColour[0]}, ${sampledColour[1]}, ${sampledColour[2]})`);
-            }
-
-            $(window).on('touchend', () => {
-                cvsElem.off('touchmove');
-
-                $('.sample_colour').css('background-color', 'rgba(0, 0, 0, 0)');
-                $('.sample_colour .black').css('display', 'inline-block');
-                $('.sample_colour .white').css('display', 'none');
-
-                isSamplingColour = false;
-            });
-
-            cvsElem.on('touchmove', sampleColourCallback);
-            sampleColourCallback(e);
         }
-
-        
     });
 }
