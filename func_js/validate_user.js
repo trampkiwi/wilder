fe3e89bc44db0eefb34f3f87535f049bde427dc5
Youngjun Lee <starwatcherkiwi@gmail.com>
2021-04-07 23:54:13 +0100

    developing profile picture drawing view

--- func_js/validate_user.js
+++ func_js/validate_user.js
@@ -4,9 +4,16 @@ firebase.auth().onAuthStateChanged((user) => {
     if(user) { // User logged in
         var db = firebase.database();
 
-        // Only logged in and 'allowed' users can access the dummy document.
-        db.ref('/user_validation_dummy').once('value').then(() => {
-            // Do nothing
+        var uid = firebase.auth().currentUser.uid;
+
+        db.ref('/users/' + uid).once('value').then((snapShot) => { // User is authorised.
+            var dat = snapShot.val();
+            
+            if(dat == null) { // User has not registered before.
+                window.location.replace('/signin_complete.m.html'); // Redirect to signup complete page.
+            } else { // User has registered before.
+                // Do nothing
+            }
         }).catch((err) => { // If error occurs
             if(err.code == 'PERMISSION_DENIED') { // Current login is invalid.
                 window.location.replace('/invalid_user.m.html'); // Redirect to invalid user page.
