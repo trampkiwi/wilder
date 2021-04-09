29cd13d8e6c1a506a5e7c6c2fb79b40281a68af7
Youngjun Lee <starwatcherkiwi@gmail.com>
2021-04-09 02:00:01 +0100

    developing explore.m.html

--- view_js/explore.m.js
+++ view_js/explore.m.js
@@ -1,49 +1,42 @@
 var firebaseDB;
 
-function initialisePage() {
+async function initialise() { // called after elements have loaded and firebase user has been recognised
+    firebaseDB = firebase.database();
+
     $('.all_content').css('display', 'block');
 
-    // put in user's profile into the profile button
+    // Set profile button image.
     $('#profilebutton').attr('src', firebase.auth().currentUser.photoURL);
 
     initialiseMap();
 }
 
-$(async function(){
-    // -------------- Check user ---------------
+// -------------- Check user ---------------
 
-    /*firebase.auth().onAuthStateChanged((user) => {
-        if(user) { // User logged in
-            var db = firebase.database();
-    
-            var uid = firebase.auth().currentUser.uid;
-    
-            db.ref('/users/' + uid).once('value').then((snapShot) => { // User is authorised.
-                var dat = snapShot.val();
+firebase.auth().onAuthStateChanged((user) => {
+    if(user) { // User logged in
+        var db = firebase.database();
+
+        var uid = firebase.auth().currentUser.uid;
+
+        db.ref('/users/' + uid).once('value').then((snapShot) => { // User is authorised.
+            var dat = snapShot.val();
+            
+            if(dat == null) { // User has not registered before.
+                window.location.replace('/signin_complete.m.html'); // Redirect to signup complete page.
+            } else { // User has registered before.
+                $(initialise);
+            }
+        }).catch((err) => { // If error occurs
+            if(err.code == 'PERMISSION_DENIED') { // Current login is invalid.
+                window.location.replace('/invalid_user.m.html'); // Redirect to invalid user page.
+            } else {
+                alert(err);
                 
-                if(dat == null) { // User has not registered before.
-                    window.location.replace('/signin_complete.m.html'); // Redirect to signup complete page.
-                } else { // User has registered before.
-                    initialiseMap();
-                }
-            }).catch((err) => { // If error occurs
-                if(err.code == 'PERMISSION_DENIED') { // Current login is invalid.
-                    window.location.replace('/invalid_user.m.html'); // Redirect to invalid user page.
-                } else {
-                    alert(err);
-                    
-                    throw err;
-                }
-            });
-        } else { // User not logged in
-            window.location.replace('/');
-        }
-    });*/
-
-    // TODO: put initialiseMap() back into the authstate check code.
-
-    firebaseDB = firebase.database();
-
-    initialisePage();
-    
+                throw err;
+            }
+        });
+    } else { // User not logged in
+        window.location.replace('/');
+    }
 });
