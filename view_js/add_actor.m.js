71b4b2959a13529b473dca1044b49a2f4052e15f
Youngjun Lee <starwatcherkiwi@gmail.com>
2021-05-15 11:15:09 +0100

    User check functionality in add_actor

---
+++ view_js/add_actor.m.js
@@ -0,0 +1,37 @@
+var firebaseDB;
+
+async function initialise() { // called after elements have loaded and firebase user has been recognised
+    firebaseDB = firebase.database();
+
+    $('.all_content').css('display', 'block');
+}
+
+// -------------- Check user ---------------
+
+firebase.auth().onAuthStateChanged((user) => {
+    if(user) { // User logged in
+        var db = firebase.database();
+
+        var uid = firebase.auth().currentUser.uid;
+
+        db.ref('/users/' + uid).once('value').then((snapShot) => { // User is authorised.
+            var dat = snapShot.val();
+            
+            if(dat == null) { // User has not registered before.
+                window.location.replace('/signin_complete.m.html'); // Redirect to signup complete page.
+            } else { // User has registered before.
+                $(initialise);
+            }
+        }).catch((err) => { // If error occurs
+            if(err.code == 'PERMISSION_DENIED') { // Current login is invalid.
+                window.location.replace('/invalid_user.m.html'); // Redirect to invalid user page.
+            } else {
+                alert(err);
+                
+                throw err;
+            }
+        });
+    } else { // User not logged in
+        window.location.replace('/');
+    }
+});
